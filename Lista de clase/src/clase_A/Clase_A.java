3dbb3faa63f1dda638b07bcc92cd1cb59d1ab42d
luis.tallafigo <luis.tallafigo@WKSALMPBB919.u-tad.es>
2016-09-28 09:42:36 +0200

    Clase A terminada

--- Lista de clase/src/clase_A/Clase_A.java
+++ Lista de clase/src/clase_A/Clase_A.java
@@ -1,7 +1,10 @@
 package clase_A;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,37 +12,45 @@ import java.util.ArrayList;
 public class Clase_A {
 	public String p = "p";
 	public ArrayList<String> listaAlumnos = new ArrayList<String>();
-	
-		//este metodo es para incluir el Arraylist en el documento de texto.
-		public void escribirFichero( ){
-			
-			String sFichero = "lista.txt";
-			File fichero = new File(sFichero);
-			
-			BufferedWriter bw = null;
+
+	// este metodo es para incluir el Arraylist en el documento de texto.
+	public void escribirFichero() {
+
+		String sFichero = "lista.txt";
+		File fichero = new File(sFichero);
+
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter(sFichero));
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		for (int x = 0; x < 10; x++) {
+
 			try {
-				bw = new BufferedWriter(new FileWriter(sFichero));
+				bw.write(x + "." + listaAlumnos.get(x) + "\n");
 			} catch (IOException e) {
-				
 				e.printStackTrace();
 			}
-			
-			for (int x=0;x<10;x++){
-				
-				try {
-					bw.write(x +"." + listaAlumnos.get(x) + "\n");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-			}
-				   
-			
-		};
-		
-		/*public x leerFichero(){
-			
-		}*/
+
+		}
+
+	};
+
+	public static void muestraContenido(String archivo) throws FileNotFoundException, IOException {
+		String cadena;
+		FileReader f = new FileReader(archivo);
+		BufferedReader b = new BufferedReader(f);
+		while ((cadena = b.readLine()) != null) {
+			System.out.println(cadena);
+		}
+		b.close();
 	}
+	
+	 /*public static void main(String[] args) throws IOException {
+	        muestraContenido("lista.txt");
+	    }*/
 
-
+}
